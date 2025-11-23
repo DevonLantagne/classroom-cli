8b175c2ac71e726404bcb2f2fdda6dc824df6f1c
Devon <devonlantagne@me.com>
2025-11-23 10:49:02 -0600

    updated CLI help strings.

--- classroom_cli/cli.py
+++ classroom_cli/cli.py
@@ -20,7 +20,14 @@ if not token:
 
 @click.group()
 def cli():
-    """GitHub Classroom assignment manager."""
+    """
+    GitHub Classroom assignment manager.
+
+    This CLI tool can clone all student repos to a selected folder.
+    You can perform a batched build of all student repositories.
+    You can easily create 'assessment' branches from their main.
+    You can also commit and push all 'assessment' branches at once.
+    """
 
 
 @cli.command()
@@ -32,10 +39,12 @@ def cli():
 )
 def clone(assignment_id, class_dir):
     """
-    Clones all student repos for an assignment. A submission directory
-    will be created in the class_dir. Student repos will be cloned into
-    the submission directory. Student repos are prepended with their
-    student names provided by the classroom roster csv file.
+    Clones all student repos for an assignment.
+
+    A submission directory will be created in the class_dir. Student
+    repos will be cloned into the submission directory. Student repos
+    are prepended with their student names provided by the classroom
+    roster csv file.
 
     Do not run this command twice for the same assignment unless some
     students did not have a repo prior to your first clone. This will
@@ -59,7 +68,13 @@ def clone(assignment_id, class_dir):
     help="Name of the branch to be created in the student's repo.",
 )
 def cloneBuildBranch(assignment_id, class_dir, branch_name):
-    """Clone, build, and create an 'Assessment' branch for all students."""
+    """
+    Runs 'clone', 'build', and 'branch' commands.
+
+    This command will run the 'clone', 'build', and 'branch' commands
+    sequentially. This is useful if you want to clone an assignment,
+    build it, and then create a branch for assessment.
+    """
     # Clone assignments just like the 'clone' command
     submissionDir = gh_classroom_clone(assignment_id, class_dir)
     # Build the assignment just like the 'build' command
@@ -70,7 +85,11 @@ def cloneBuildBranch(assignment_id, class_dir, branch_name):
 
 @cli.command()
 def roster():
-    """Print the roster dictionary."""
+    """
+    Print the roster dictionary.
+
+    Prints the roster that you provided with your roster_csv .env variable.
+    """
     roster = load_roster(roster_csv)
     for username, identifier in roster.items():
         click.echo(f"{identifier}: {username}")
@@ -81,7 +100,13 @@ def roster():
     "--submission-dir", default=".", help="Directory containing student repos."
 )
 def build(submission_dir):
-    """Build all PlatformIO projects in an submission directory."""
+    """
+    Build all projects in an submission directory.
+
+    This command calls your PlatformIO build command for each student project.
+    This command is useful if you want to quickly check which project earned
+    an automatic 'zero' grade.
+    """
     build_all(submission_dir)
 
 
@@ -95,7 +120,15 @@ def build(submission_dir):
     help="Name of the branch to be created in the student's repo.",
 )
 def branch(submission_dir, branch_name):
-    """Creates an 'assessment' branch in all student repos in a submission directory."""
+    """
+    Creates an 'assessment' branch in all student repos.
+
+    Assessing on the main branch is not recommended as students like to forget
+    to push their most recent changes. Creating an assessment branch allows
+    students to push thier most recent changes without your assessment causing
+    merge conflicts.
+
+    """
     changeBranch(submission_dir, branch_name)
 
 
@@ -115,8 +148,11 @@ def branch(submission_dir, branch_name):
 )
 def push(submission_dir, message, branch_name):
     """
-    Stage all changes, commit, and push the 'assessment' branch
-    for every student repo in a submission directory.
+    Stage all changes, commit, and push to origin.
+
+    This command will iterate through each studentn repo and
+    stage all your assessment changes, commit them with the provided
+    commit message and push to origin with the provided branch name.
     """
     push_branches(submission_dir, message, branch_name)
 
